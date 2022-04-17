65a8d1fd16d1f2708fc7bc4e4003582d9100a472
Robert Kędzior <rob.kedzior@hotmail.com>
2022-04-17 21:46:16 +0200

    finished showcase

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { Features, Download, SectionWrapper} from "./components";
+import assets from './assets'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <SectionWrapper 
+        title="Your own store of Nifty NFTs. Start Selling & Growing"
+        description="Buy, Store, Collect NFTs. Exchange & earn crypto. Join 25+ million people using ProfNet Marketplace"
+        showBtn
+        mockupImg={assets.homeHero}
+        banner="banner"
+      />
+       <SectionWrapper 
+        title="Smart User Interface Marketplace"
+        description="Experience a buttery UI of ProNef NFT Marketplace. Smooth constant colors of a fluent UI design."
+        mockupImg={assets.homeCards}
+        reverse
+      />
+      <Features />
+      <SectionWrapper 
+        title="Deployment"
+        description="ProNef is built using Expo which runs natively on all users' devices. You can easily get your app into people's hands"
+        mockupImg={assets.feature}
+        reverse
+      />
+      <SectionWrapper 
+        title="Creative way to showcase the store"
+        description="The app contains two screens. The first screen lists all NFTs while the second one shows the details of a specific NFT."
+        mockupImg={assets.mockup}
+        banner="banner02"
+      />
+      <Download />
+    </>
   );
 }
 
---
+++ src/components/SectionWrapper.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import styles from '../styles/Global'
+import assets from '../assets'
+import {Button} from './index'
+
+const SectionWrapper = ({title, description, showBtn, mockupImg, banner}) => {
+  return (
+    <div className={`min-h-screen ${styles.section} ${styles.bgWhite} ${banner}`}>
+      <div className={`flex items-center ${styles.boxClass} w-11/12 sm:w-full minmd:w3/4`}>
+        <div className={`${styles.descDiv} fadeLeftMini`}>
+        <h1 className={`${styles.h1Text}`}>{title}</h1>
+        <p className={`${styles.descriptionText}`}>{description}</p>
+        {showBtn && (
+          <Button 
+            assetUrl={assets.expo}
+            link="deployed NFT marketplace rn"
+          />
+        )}
+        </div>
+        <div className={`flex-1 ${styles.flexCenter} p-8 sm:px-9`}>
+          <img src={mockupImg} alt="mockup" className={styles.sectionImg}/>
+      </div>
+      </div>
+    </div>
+  )
+}
+
+export default SectionWrapper
